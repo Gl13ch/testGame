de6650c21dcb582e44efb73ab39d6356bb00094f
SamOstrander <samostr@gmail.com>
2024-03-30 17:35:36 -0500

    added 2 player animations

--- GodotTheRobot.gd
+++ GodotTheRobot.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
 var BOUNCE_VELOCITY = -1000.0
+@onready var animation_player = $AnimationPlayer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,6 +16,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animation_player.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -36,5 +38,6 @@ func _on_red_enemy_hitbox_body_entered(body):
 func _on_bounce_pad_body_entered(body):
 	if body == self:
 		velocity.y = BOUNCE_VELOCITY
+		animation_player.play("bouncepad_launch")
 		print("bounce")
 		pass # Replace with function body.
