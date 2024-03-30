c7bc00c4cee797366843d5af9801d0e6f8d1622b
SamOstrander <samostr@gmail.com>
2024-03-30 13:18:07 -0500

    fixd badguy movement

--- RedEnemy.gd
+++ RedEnemy.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-var motion = Vector2( )
+const SPEED = 200.0
+#var motion = Vector2( )
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = 1
 
 func _physics_process(delta):
-	motion.y += gravity
-	motion.x += SPEED * direction
+	velocity.y += gravity
+	velocity.x = -SPEED * direction
 
 	move_and_slide()
