4d775e662c6820c9d33f74e5c26934ad18235978
SamOstrander <samostr@gmail.com>
2024-03-31 16:58:57 -0500

    branched teleport as scene

--- Teleport1.gd
+++ Teleport1.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var target = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,4 +14,4 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		body.position = Vector2(1142,576)
+		body.position = target#Vector2(1142,576)
